a1aca4cc9cb4a63ec1901ca1b2177386f3f0ce0e
itsjackie00 <jackiedanso33@gmail.com>
2024-03-14 17:26:26 +0100

    added assigment two

--- js/script..js
+++ js/script..js
@@ -27,3 +27,13 @@ function invertiParola(str){
   const strInversa = str.toLowerCase().split('').reverse().join('');  
   return strInversa;
 }
+
+
+/*
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
